1386a436497d416faf81add08c8a797df7f8fdc8
te618 <stewarttelly10@gmail.com>
2025-03-07 11:03:32 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom']
+  },
+  define: {
+    'process.env': {}
+  }
+}) 
